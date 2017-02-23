1c4031689a51121dffa69a32fb84f9b486467043
Marc Vertido <mvertido@pillartechnology.com>
2017-02-23 15:25:50 -0500

    Used jsonPath instead of domain objects in integration test

--- src/test/java/org/doge/controllers/TodosControllerIntegrationTest.java
+++ src/test/java/org/doge/controllers/TodosControllerIntegrationTest.java
@@ -1,10 +1,5 @@
 package org.doge.controllers;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.doge.domain.TodoDomain;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -31,18 +23,12 @@ public class TodosControllerIntegrationTest {
 
     @Test
     public void getTodos() throws Exception {
-        MvcResult result = mvc.perform(get("/todos"))
+        mvc.perform(get("/todos"))
             .andExpect(status().isOk())
-            .andReturn();
-
-        JsonParser parser = new JsonFactory().createParser(result.getResponse().getContentAsString());
-        ObjectMapper mapper = new ObjectMapper();
-        List<TodoDomain> todosReturned = Arrays.asList(mapper.readValue(parser, TodoDomain[].class));
-
-        List<TodoDomain> expectedTodos = Arrays.asList(
-            new TodoDomain(1, "This is the first Todo", false),
-            new TodoDomain(2, "This is a completed Todo", true)
-        );
-        Assert.assertThat(todosReturned, is(expectedTodos));
+            .andExpect(jsonPath("$", hasSize(2)))
+            .andExpect(jsonPath("[?(@.id == 1)].name", containsInAnyOrder("This is the first Todo")))
+            .andExpect(jsonPath("[?(@.id == 1)].completed", containsInAnyOrder(false)))
+            .andExpect(jsonPath("[?(@.id == 2)].name", containsInAnyOrder("This is a completed Todo")))
+            .andExpect(jsonPath("[?(@.id == 2)].completed", containsInAnyOrder(true)));
     }
 }
