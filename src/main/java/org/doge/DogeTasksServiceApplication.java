291ed1761f2fffcb2d9a554d7f90e23504c909ff
Marc Vertido <mvertido@pillartechnology.com>
2017-03-01 12:24:07 -0500

    Made service deployable into an application server (at least in Tomcat)

--- src/main/java/org/doge/DogeTasksServiceApplication.java
+++ src/main/java/org/doge/DogeTasksServiceApplication.java
@@ -2,9 +2,16 @@ package org.doge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DogeTasksServiceApplication {
+public class DogeTasksServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DogeTasksServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DogeTasksServiceApplication.class, args);
