8c134958d424ca7cf5fd9f6766a3d754a9d50122
Marc Vertido <mvertido@pillartechnology.com>
2017-03-03 16:44:32 -0500

    Installing Tomcat, but test is still failing...

--- packer/scripts/install_dependencies.sh
+++ packer/scripts/install_dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y openjdk-8-jre
+sudo apt-get update && sudo apt-get install -y openjdk-8-jre tomcat8
+
+cat > ~/.bashrc <<"EOF"
+export CATALINA_HOME=/usr/share/tomcat8
+EOF
--- packer/tests/spec/localhost/dependencies_spec.rb
+++ packer/tests/spec/localhost/dependencies_spec.rb
@@ -4,4 +4,8 @@ describe 'doge-tasks-service dependencies' do
   describe command('java -version') do
     its(:stderr) { should contain 'openjdk version "1.8' }
   end
+
+  describe command('$CATALINA_HOME/bin/catalina.sh version') do
+    its(:stderr) { should contain 'Apache Tomcat/8.0'}
+  end
 end
