9fd048b800aa4cb4b40b17ae34a7aaeba3663747
Remington Davenport <remington.davenport@gmail.com>
2017-03-13 11:43:27 -0400

    DOGE-22: Maisam/Remy - fixed quotes on export command

--- packer/scripts/install_dependencies.sh
+++ packer/scripts/install_dependencies.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 function echo_catalina_home() {
-    echo `export CATALINA_HOME=/usr/share/tomcat8`
+    echo 'export CATALINA_HOME=/usr/share/tomcat8'
 }
 
 sudo apt-get update && sudo apt-get install -y openjdk-8-jre tomcat8
 
 echo_catalina_home >> ~/.bash_profile
 echo_catalina_home >> /home/ubuntu/.bash_profile
-
